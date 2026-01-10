fc0553c521494cfab6e4bd4f33c0f72aebb961d2
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2026-01-10 21:38:35 +0200

    feat: add SignInFragment

--- app/src/main/java/com/arhizmei/daybydayhabittracker/auth/AuthActivity.kt
+++ app/src/main/java/com/arhizmei/daybydayhabittracker/auth/AuthActivity.kt
@@ -1,10 +1,11 @@
-package com.arhizmei.daybydayhabittracker
+package com.arhizmei.daybydayhabittracker.auth
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arhizmei.daybydayhabittracker.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
