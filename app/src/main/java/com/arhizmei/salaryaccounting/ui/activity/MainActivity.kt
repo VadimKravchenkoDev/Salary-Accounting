fb993168ff9645e2f396a0d1569129e0331163dc
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2026-01-27 18:49:46 +0200

    feat: add mainActivity

--- app/src/main/java/com/arhizmei/salaryaccounting/ui/activity/MainActivity.kt
+++ app/src/main/java/com/arhizmei/salaryaccounting/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.arhizmei.salaryaccounting.ui.auth
+package com.arhizmei.salaryaccounting.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -6,21 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.arhizmei.salaryaccounting.R
-import com.arhizmei.salaryaccounting.ui.signin.SignInFragment
 
-class AuthActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_auth)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(
-                systemBars.left,
-                systemBars.top,
-                systemBars.right,
-                systemBars.bottom
-            )
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
