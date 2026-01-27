6ba258deeb53e1eb98d61e8f654dfe7e3c70df2f
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2026-01-27 20:37:45 +0200

    feat: nav.menu works now

--- app/src/main/java/com/arhizmei/salaryaccounting/ui/activity/MainActivity.kt
+++ app/src/main/java/com/arhizmei/salaryaccounting/ui/activity/MainActivity.kt
@@ -6,20 +6,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.arhizmei.salaryaccounting.R
+import com.arhizmei.salaryaccounting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val navController = findNavController(R.id.main_nav_host)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.main_nav_host) as NavHostFragment
+
+        val navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
+        binding.bottomNav.itemIconTintList = null
+
     }
 }
