881c469eec2634147bdbda44f23cc2892f1d89cb
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2026-02-10 16:34:33 +0200

    feat: add CalculatorViewModel

--- app/src/main/java/com/arhizmei/salaryaccounting/ui/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/arhizmei/salaryaccounting/ui/calculator/CalculatorFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.arhizmei.salaryaccounting.R
 import com.arhizmei.salaryaccounting.databinding.FragmentCalculatorBinding
 
 class CalculatorFragment : Fragment() {
     private var _binding: FragmentCalculatorBinding? = null
     private val binding get() = _binding!!
+    private val viewModel : CalculatorViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +29,16 @@ class CalculatorFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentCalculatorBinding.bind(view)
+
+        binding.calculateBt.setOnClickListener {
+            val salary = binding.salaryAmountEdit.text.toString().toIntOrNull() ?:0
+            val hours = binding.hoursOfWeekEdit.text.toString().toIntOrNull() ?:0
+            val days = binding.dayOfWeekEdit.text.toString().toIntOrNull() ?:0
+
+            viewModel.setData(salary, hours, days)
+
+            findNavController().navigate(R.id.action_ca)
+        }
 
     }
 
---
+++ app/src/main/java/com/arhizmei/salaryaccounting/ui/calculator/CalculatorViewModel.kt
@@ -0,0 +1,21 @@
+package com.arhizmei.salaryaccounting.ui.calculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.arhizmei.salaryaccounting.ui.data.CalculatorData
+
+class CalculatorViewModel(
+    private val state: SavedStateHandle
+) : ViewModel() {
+    companion object {
+        private const val KEY_DATA = "calc_data"
+    }
+
+    val data: LiveData<CalculatorData> = state.getLiveData(KEY_DATA)
+
+    fun setData(salary: Int, hours: Int, days: Int) {
+        state[KEY_DATA] = CalculatorData(salary, hours, days)
+    }
+}
